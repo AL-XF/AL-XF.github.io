edee889fdd34f190f8f30606b2039134dd078044
AL-XF <30537599+AL-XF@users.noreply.github.com>
2017-11-03 16:17:14 -0400

    Add files via upload

--- js/chemical_chart.js
+++ js/chemical_chart.js
@@ -240,9 +240,11 @@ var Chart = (function(window,d3) {
     {
            var query = window.location.search.substring(1);
            var vars = query.split('&');
-           for (var i=0;i<vars.length;i++) {
-                   var pair = vars[i].split('=');
-                   if(pair[0] == variable){return pair[1];}
+           for (var i=0; i<vars.length; i++) {
+                   var pair = decodeURI(vars[i]).split('=');
+                   if(pair[0] == variable){
+                       return pair[1];
+                    }
            }
            return defaultValue;
     }
